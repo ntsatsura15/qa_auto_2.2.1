552c251cd37df8508cf139aec209e7a595d556bb
ntsatsura15 <n.tsatsura15@gmail.com>
2024-09-16 02:07:31 +0200

    Added script2.js, changed in readme.md for hw-2.2.4

---
+++ script2.js
@@ -0,0 +1,23 @@
+// Инкремент
+
+// Пример 1
+let inc1 = 5;
+inc1++;
+console.log(inc1);
+
+// Пример 2
+let inc2 = 5;
+++inc2;
+console.log(inc2);
+
+// Декремент
+
+// Пример 1
+let decr1 = 5;
+decr1--;
+console.log(decr1);
+
+// Пример 2
+let decr2 = 5;
+--decr2;
+console.log(decr2);
