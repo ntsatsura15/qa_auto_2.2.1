d25388d3d08ba702291c1316c5d0eb6b7d00f2d9
ntsatsura15 <n.tsatsura15@gmail.com>
2024-09-16 08:42:20 +0200

    Corrected hw-2.2.4

--- script2.js
+++ script2.js
@@ -1,23 +1,64 @@
 // Инкремент
 
 // Пример 1
-let inc1 = 5;
-inc1++;
-console.log(inc1);
+function exampleIncrement1(inc1) {
+  inc1++;
+  return inc1;
+}
+
+console.log(exampleIncrement1(5));
 
 // Пример 2
-let inc2 = 5;
-++inc2;
-console.log(inc2);
+function exampleIncrement2(inc2) {
+  ++inc2;
+  return inc2;
+}
+
+console.log(exampleIncrement2(5));
+
+// Пример 3
+function exampleIncrement3(inc3) {
+  return inc3++;
+}
+
+console.log(exampleIncrement3(5));
+
+// Пример 4
+function exampleIncrement4(inc4) {
+  return ++inc4;
+}
+
+console.log(exampleIncrement4(5));
 
 // Декремент
 
 // Пример 1
-let decr1 = 5;
-decr1--;
-console.log(decr1);
+function exampleDecrement1(decr1) {
+  decr1--;
+  return decr1;
+}
+
+console.log(exampleDecrement1(5));
 
 // Пример 2
-let decr2 = 5;
---decr2;
-console.log(decr2);
+function exampleDecrement2(decr2) {
+  --decr2;
+  return decr2;
+}
+
+console.log(exampleDecrement2(5));
+
+// Пример 3
+function exampleDecrement3(decr3) {
+  return decr3--;
+}
+
+console.log(exampleDecrement3(5));
+
+// Пример 4
+function exampleDecrement4(decr4) {
+  return --decr4;
+}
+
+console.log(exampleDecrement4(5));
+
